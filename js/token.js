7ed8556e84e1ca2791b7d83b8ff111b1a2713147
ahsonkhan <ahson_ahmedk@yahoo.com>
2016-03-16 11:55:28 -0400

    Reverting back to static

--- js/token.js
+++ js/token.js
@@ -1,21 +1,16 @@
 $(document).ready(function(){
     $("#genToken").click(function(){
-		function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-		}
-		var tokenText = s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+		
+		var tokenText = "3a2993df-e8a2-11e5-9f31-0228a770e05b";
         $("#token").text(tokenText);
 	
-			$.ajax({
+			/*$.ajax({
                 url: "../token/store_token.php",
                 type: "POST",
                 data: {
                     token:tokenText
                 },
                 cache: false
-            })
+            })*/
 	});
 });
